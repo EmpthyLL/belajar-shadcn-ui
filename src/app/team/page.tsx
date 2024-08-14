7ef2e95e6e4259cf72aacca710ddcc8645eb1bd7
howard <javileh40@gmail.com>
2024-08-14 17:04:33 +0700

    Intinya perbaikan

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,23 +1,14 @@
 'use client'
 import { Button } from "@/components/ui/button";
-import { OctagonAlert, Plus, Trash2 } from "lucide-react";
+import { OctagonAlert, Trash2 } from "lucide-react";
 import { useEffect, useState } from "react";
 import UserItem from 'useritem'
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import { Badge } from "@/components/ui/badge";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 import axios from "axios";
 import SearchInput from "@/components/search";
+import DialogForm from "@/components/dialog";
 
 type members = {
     email:string,
@@ -140,45 +131,8 @@ export default function Team() {
                     {/* <Command className="rounded-lg border">
                     <CommandInput placeholder="Type an email to invite..." />
                     </Command> */}
-                    <SearchInput type="text" placeholder='Search a team member...'/>    
-                    <Dialog>
-                    <DialogTrigger asChild>
-                        <Button variant={'outline'} className="flex justify-center items-end"><Plus /> Add Members</Button>
-                    </DialogTrigger>
-                    <DialogContent className="sm:max-w-[425px]">
-                        <DialogHeader>
-                        <DialogTitle>Add Member</DialogTitle>
-                        <DialogDescription>
-                            Make changes to your profile here. Click save when you're done.
-                        </DialogDescription>
-                        </DialogHeader>
-                        <div className="grid gap-4 py-4">
-                        <div className="grid grid-cols-4 items-center gap-4">
-                            <Label htmlFor="name" className="text-right">
-                            Name
-                            </Label>
-                            <Input
-                            id="name"
-                            defaultValue="Pedro Duarte"
-                            className="col-span-3"
-                            />
-                        </div>
-                        <div className="grid grid-cols-4 items-center gap-4">
-                            <Label htmlFor="username" className="text-right">
-                            Username
-                            </Label>
-                            <Input
-                            id="username"
-                            defaultValue="@peduarte"
-                            className="col-span-3"
-                            />
-                        </div>
-                        </div>
-                        <DialogFooter>
-                        <Button type="submit">Save changes</Button>
-                        </DialogFooter>
-                    </DialogContent>
-                    </Dialog>
+                    <SearchInput type="text" placeholder='Search a team member...'/>
+                    <DialogForm/>
                 </div>
             </div>
             <div className="border rounded-md">
