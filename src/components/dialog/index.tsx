3f5a0839b199eb04032dcc722fe1ec987c3d9835
EmpthyLL <javileh40@gmail.com>
2024-08-15 08:40:24 +0700

    Form

--- src/components/dialog/index.tsx
+++ src/components/dialog/index.tsx
@@ -1,60 +1,85 @@
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-  } from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Button } from "../ui/button";
 import { Plus } from "lucide-react";
 export default function DialogForm() {
-    return (
-        
-        <Dialog>
-        <DialogTrigger asChild>
-            <Button variant={'outline'} className="flex justify-center items-end"><Plus /> Add Members</Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-            <DialogHeader>
-            <DialogTitle>Add Member</DialogTitle>
-            <DialogDescription>
-                Make changes to your profile here. Click save when you're done.
-            </DialogDescription>
-            </DialogHeader>
-            <div className="grid gap-4 py-4">
-            <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="username" className="text-right">
-                username
-                </Label>
-                <Input
-                id="username"
-                className="col-span-3"
-                required
-                placeholder="Username"
-                name="username"
-                />
-            </div>
-            <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="email" className="text-right">
-                Email
-                </Label>
-                <Input
-                id="username"
-                className="col-span-3"
-                required
-                placeholder="Email"
-                name="email"
-                />
-            </div>
-            </div>
-            <DialogFooter>
-            <Button type="submit">Save changes</Button>
-            </DialogFooter>
-        </DialogContent>
-        </Dialog>
-    );
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant={"outline"} className="flex justify-center items-end">
+          <Plus /> Add Members
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Add Member</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when you're done.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-right">
+              Username
+            </Label>
+            <Input
+              id="username"
+              className="col-span-3"
+              required
+              placeholder="Username"
+              name="username"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="email" className="text-right">
+              Email
+            </Label>
+            <Input
+              id="username"
+              className="col-span-3"
+              required
+              placeholder="Email"
+              name="email"
+            />
+          </div>
+          <Select name="role">
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Select your role" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Fruits</SelectLabel>
+                <SelectItem value="apple">Apple</SelectItem>
+                <SelectItem value="banana">Banana</SelectItem>
+                <SelectItem value="blueberry">Blueberry</SelectItem>
+                <SelectItem value="grapes">Grapes</SelectItem>
+                <SelectItem value="pineapple">Pineapple</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
 }
