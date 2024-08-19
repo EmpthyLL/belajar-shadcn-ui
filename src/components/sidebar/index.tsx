f627c1687c6155950b85c0dcbfd51db6683c4863
howard <javileh40@gmail.com>
2024-08-19 10:53:06 +0700

    Intinya perbaikan

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,6 +1,14 @@
-'use client'
+"use client";
 
-import { Bell, CreditCard, Inbox, MessageSquare, Settings, ShieldCheck, User } from "lucide-react";
+import {
+  Bell,
+  CreditCard,
+  Inbox,
+  MessageSquare,
+  Settings,
+  ShieldCheck,
+  User,
+} from "lucide-react";
 import UserItemCustom from "../user-item";
 import {
   Command,
@@ -12,95 +20,100 @@ import {
   CommandList,
   CommandSeparator,
   CommandShortcut,
-} from "@/components/ui/command"
-import { Suspense } from "react";
-import LoadingMenu from "./loading";
-import UserItem from "useritem"
+} from "@/components/ui/command";
+import UserItem from "useritem";
 import Link from "next/link";
 
 export default function Sidebar() {
-    const menuList = [
+  const menuList = [
+    {
+      group: "General",
+      items: [
         {
-            group: 'General',
-            items:[
-                {
-                    link:'/',
-                    text:'Profile',
-                    icon:<User/>
-
-                },
-                {
-                    link:'/',
-                    text:'Inbox',
-                    icon:<Inbox />
-                },
-                {
-                    link:'/',
-                    text:'Billing',
-                    icon:<CreditCard />
-                },
-                {
-                    link:'/',
-                    text:'Notifications',
-                    icon:<Bell />
-                },
-            ]
+          link: "/",
+          text: "Profile",
+          icon: <User />,
         },
         {
-            group: 'Settings',
-            items:[
-                {
-                    link:'/',
-                    text:'General Settings',
-                    icon:<Settings />
-                },
-                {
-                    link:'/',
-                    text:'Privacy',
-                    icon:<ShieldCheck/>
-                },
-                {
-                    link:'/',
-                    text:'Logs',
-                    icon:<MessageSquare />
-                },
-            ]
+          link: "/",
+          text: "Inbox",
+          icon: <Inbox />,
         },
-    ]
-    return (
-        <div className={`gap-2 flex flex-col min-h-screen p-4 border-r`}>
-            <div>
-                <UserItemCustom/>
-                {/* <UserItem
+        {
+          link: "/",
+          text: "Billing",
+          icon: <CreditCard />,
+        },
+        {
+          link: "/",
+          text: "Notifications",
+          icon: <Bell />,
+        },
+      ],
+    },
+    {
+      group: "Settings",
+      items: [
+        {
+          link: "/",
+          text: "General Settings",
+          icon: <Settings />,
+        },
+        {
+          link: "/",
+          text: "Privacy",
+          icon: <ShieldCheck />,
+        },
+        {
+          link: "/",
+          text: "Logs",
+          icon: <MessageSquare />,
+        },
+      ],
+    },
+  ];
+  return (
+    <div className={`gap-2 flex flex-col min-h-screen p-4 border-r`}>
+      <div>
+        <UserItemCustom />
+        {/* <UserItem
                     name='Sarah Pangestia'
                     description='sarapang12@gmail.com'
                     shadow={false}
                     avatarUrl='SP.png'
                     online={true}
                 /> */}
-            </div>
-            <div className="grow">
-            <Command className="overflow-visible">
-            <CommandList className="overflow-visible">
-                {menuList.map((menu, key) => {
-                    return (menu.items ? <div key={key} >
-                        <CommandGroup heading={menu.group}>
-                            {
-                                menu.items.map((item, key) => <CommandItem key={key} className="flex gap-2 cursor-pointer">{item.icon}{item.text}</CommandItem>)
-                            }
-                        </CommandGroup>
-                        <CommandSeparator />
-                    </div> : null)
-                })}
-            </CommandList>
-            </Command>
-            </div>
-            <div className="text-sm text-gray-600">
-                <Link href='/team' className="flex items-center gap-2">
-                <Settings />
-                <span>Team Settings</span>
-                </Link>
-            </div>
-        </div>
-    );
+      </div>
+      <div className="grow">
+        <Command className="overflow-visible">
+          <CommandList className="overflow-visible">
+            {menuList.map((menu, key) => {
+              return menu.items ? (
+                <div key={key}>
+                  <CommandGroup heading={menu.group}>
+                    {menu.items.map((item, key) => (
+                      <CommandItem
+                        key={key}
+                        className="flex gap-2 cursor-pointer"
+                      >
+                        {item.icon}
+                        {item.text}
+                      </CommandItem>
+                    ))}
+                  </CommandGroup>
+                  <CommandSeparator />
+                </div>
+              ) : null;
+            })}
+          </CommandList>
+        </Command>
+      </div>
+      <div className="text-sm text-gray-600">
+        <Link href="/team" className="flex items-center gap-2">
+          <Settings />
+          <span>Team Settings</span>
+        </Link>
+      </div>
+    </div>
+  );
 }
